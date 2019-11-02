683fe259a118184524740f248053c3d235bb0cd5
EricHuang950313 <45135711+EricHuang950313@users.noreply.github.com>
2019-11-02 20:52:40 +0800

    Update ch4-7.py

--- InClass/CH4_Course/ch4-7.py
+++ InClass/CH4_Course/ch4-7.py
@@ -1,14 +1,18 @@
+import math
 class fraction():
     def __init__(self, numerator, denominator):
         self.n = numerator
         self.d = denominator
         pass
+
     def __str__(self):
         return ("{}/{}".format(self.n, self.d))
+
     def __add__(self, other):
         n = self.n*other.d + other.n*self.d
         d = self.d*other.d
         return ("{}/{}".format(n, d))
+
     def Devide(self, A, B):
         C = max(A, B)
         D = min(A, B)
@@ -18,11 +22,16 @@ class fraction():
             if D%(i+1)==0 and C%(i+1)==0:
                 if (i+1)>temp:
                     temp = (i+1)
-
+        return temp
 
 
 F1 = fraction(2, 3)
 F2 = fraction(1, 9)
 
-print(F1)
 print(F1 + F2)
+before = F1 + F2
+x = math.floor(len(before)/2)
+A = int(before[:x])
+B = int(before[x+1:])
+temp = F1.Devide(A, B)
+print("{:.0f}/{:.0f}".format(A/temp, B/temp))
