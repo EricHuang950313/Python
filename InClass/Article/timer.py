fc91923aa71666f6a34b2de8a41dab424315671e
EricHuang950313 <45135711+EricHuang950313@users.noreply.github.com>
2020-02-13 22:51:34 +0800

    Update timer.py

--- InClass/Article/timer.py
+++ InClass/Article/timer.py
@@ -1,20 +1,20 @@
 import tkinter as tk 
 import time 
+from functools import partial
+
 def main(): 
+    global window, l
     window = tk.Tk()
     window.title("Timer")
     window.geometry("600x400+0+0")
-    timer(window)
+    l = tk.Label(window, font=("微軟正黑體",50, "bold"))
+    l.pack()
+    update()
     window.mainloop()
     
-def timer(window):
-    t = time.strftime('%H:%M:%S')
-    s = tk.StringVar()
-    l = tk.Label(window, textvariable=s, font=("微軟正黑體",50, "bold"))
-    l.pack()
-    while True:
-        t = time.strftime('%H:%M:%S')
-        s.set(t)
-        window.update()
+def update():
+    now = time.strftime("%H:%M:%S")
+    l.configure(text=now)
+    window.after(1000, update)
 
 main()
