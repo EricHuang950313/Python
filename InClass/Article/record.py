ce96c1e1e6c8a91fabed5258ea1f57cbfae417b6
EricHuang950313 <45135711+EricHuang950313@users.noreply.github.com>
2020-02-17 11:56:07 +0800

    Update record.py

--- InClass/Article/record.py
+++ InClass/Article/record.py
@@ -10,7 +10,7 @@ def main():
     window.mainloop()
 
 def menu(window):
-    global name, MEL, MEE, MEB, tss, l2, l, times
+    global name, MEL, MEE, MEB, tss, l2, l, times, iiii
     MEL = tk.Label(window, text="新增檔案", font=("微軟正黑體",20), )
     MEL.place(x=5, y=20)
     name = tk.StringVar()
@@ -21,23 +21,29 @@ def menu(window):
     if tss == 0 or l == "":
         with open("./OpenFile.txt", "r", encoding="utf-8") as file:
             a = list([file.read()])
-            if a == ['']:
+            if a == ["['']"] :
                 a.clear()
+                iiii = -1
             else:
                 l2 = a[0][2:-2].split("\', \'")
-            for i in range(len(l2)):
-                MAB = tk.Button(window, text=l2[i], font=("微軟正黑體",20), relief="flat", command=partial(show, window, times))
-                MAB.place(x=5, y=60+(i*60))
-                l += [MAB]
-                times += 1
+                for i in range(len(l2)):
+                    MAB = tk.Button(window, text=l2[i], font=("微軟正黑體",20), relief="flat", command=partial(show, window, times))
+                    MAB.place(x=5, y=60+(i*60))
+                    l += [MAB]
+                    times += 1
+                #times -= 1
+                iiii = times-1
     else:
         for i in range(len(l)):
             l[i].place(x=5, y=60+(i*60))
+        iiii = i
     tss += 1
 
 def make(window):
-    global times, l, l2, MAB
+    global times, l, l2, MAB, tss, ts, iiii
     nameExist = False
+    checkNext2 = True
+    do = True
     for i in l2:
         if name.get() == i:
             nameExist = True
@@ -45,25 +51,70 @@ def make(window):
         else:
             nameExist = False
             continue
-    if name.get() == "":
-        tk.messagebox.showinfo(title="InputError", message="請輸入檔案名稱!")
-    elif nameExist == True:
-        tk.messagebox.showinfo(title="InputError", message="輸入重複檔案名稱!")
-        name.set("")
-    else:
-        MAB = tk.Button(window, text=name.get(), font=("微軟正黑體",20), relief="flat", command=partial(show, window, times))
-        MAB.place(x=5, y=60+(times*60))
-        with open("./"+name.get()+".txt", "a", encoding="utf-8"):
-            pass
-        with open("./OpenFile.txt", "w", encoding="utf-8") as file:
-            file.truncate()
-        l += [MAB]
-        l2 += [name.get()]
-        with open("./OpenFile.txt", "a", encoding="utf-8") as file:
-            file.write(str(l2))
-        name.set("")
-        times += 1
+    try:
+        if name.get()[0]+name.get()[1]+name.get()[2]+name.get()[3] == "del,":
+            try:
+                g = int(name.get()[4:])
+            except BaseException:
+                tk.messagebox.showinfo(title="InputError", message="請輸入數字!")
+                checkNext2 = False          
+            finally:
+                if checkNext2 == True:
+                    if name.get() == "del,":
+                        tk.messagebox.showinfo(title="InputError", message="請輸入要刪除的列!")
+                        do = False
+                    elif int(name.get()[4:]) > len(l2):
+                        tk.messagebox.showinfo(title="InputError", message="輸入了無效的數字!")
+                        do = False
+                    elif int(name.get()[4:]) < 0:
+                        tk.messagebox.showinfo(title="InputError", message="輸入了無效的數字!")
+                        do = False
+                    else:
+                        l2.pop(int(name.get()[4:]) - 1)
+                        l[int(name.get()[4:]) - 1].destroy()
+                        l.pop(int(name.get()[4:]) - 1)
+                        name.set("")
+                        window.update()
+                        for i in range(len(l)):
+                            l[i].place(x=5, y=60+(i*60))
+                        with open("./OpenFile.txt", "w", encoding="utf-8") as file:
+                            file.truncate()
+                        with open("./OpenFile.txt", "a", encoding="utf-8") as file:
+                            file.write(str(l2))
+                        do = False
+                        tss = 0
+                        ts = 0
+                        times = 0
+                        l = []
+                        l2 = []
+                        tk.messagebox.showinfo(title="資料整理", message="系統資料整理,將重新啟動!")
+                        window.destroy()
+                        main()
 
+    except BaseException:
+        pass
+    finally:
+        if do == True:
+            if name.get() == "":
+                tk.messagebox.showinfo(title="InputError", message="請輸入檔案名稱!")
+            elif nameExist == True:
+                tk.messagebox.showinfo(title="InputError", message="輸入重複檔案名稱!")
+                name.set("")
+            else:
+                MAB = tk.Button(window, text=name.get(), font=("微軟正黑體",20), relief="flat", command=partial(show, window, times))
+                MAB.place(x=5, y=60+((iiii+1)*60))
+                iiii += 1
+                with open("./"+name.get()+".txt", "a", encoding="utf-8"):
+                    pass
+                with open("./OpenFile.txt", "w", encoding="utf-8") as file:
+                    file.truncate()
+                l += [MAB]
+                l2 += [name.get()]
+                with open("./OpenFile.txt", "a", encoding="utf-8") as file:
+                    file.write(str(l2))
+                name.set("")
+                times += 1
+                
 def show(window, times):
     global SL, ST, SE, SB, SB2, ts
     for i in range(len(l)):
