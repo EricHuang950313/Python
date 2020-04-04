a4ca46c3c1bb0d868cd0b308141f85c6424a57cb
EricHuang950313 <45135711+EricHuang950313@users.noreply.github.com>
2020-04-04 19:41:43 +0800

    Update d.py

--- Python01/d.py
+++ Python01/d.py
@@ -1,7 +1,14 @@
+a = 10
+b = 90
+print(a, b)
+a, b = b, a
+print(a, b)
+print("========")
+print(ord("a"), ord("A"))
+print("========")
 x = 60
 if x >= 60:
     print("1")
-print("======")
 y = 60
 if y > 60:
     print("1")
