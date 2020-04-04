146e1cbdd9cef049020c894f3a8b59dfc5b866f4
EricHuang950313 <45135711+EricHuang950313@users.noreply.github.com>
2020-04-04 20:39:15 +0800

    Update q.py

--- Python01/q.py
+++ Python01/q.py
@@ -3,6 +3,9 @@ print(num_list)
 print("=======")
 scores = [62, 60, 98, 100, 32, 11, 97]
 allscores = [i for i in scores]
+# allscores = scores.copy()
 print(allscores)
-f = [i for i in scores if i < 60]
-print(f)
+la = [i for i in scores if i < 60]
+print(la)
+lb = [i+j for i in range(1, 6)for j in range(10,16)]
+print(lb)
