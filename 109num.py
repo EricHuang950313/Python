b20f8471e7d067e62613f22607049d184f57dec1
EricHuang950313 <songzhi313@gmail.com>
2020-07-04 15:34:52 +0800

    7/4

--- 109num.py
+++
@@ -1,7 +0,0 @@
-a = 107231
-b = 0
-for i in range(31):
-    a += 1
-    b += 1
-    print(str(a) + str("@gwjh.hc.edu.tw") ,end=" ~~~ ")
-    print(b)
---
+++ Other/109num.py
@@ -0,0 +1,7 @@
+a = 107231
+b = 0
+for i in range(31):
+    a += 1
+    b += 1
+    print(str(a) + str("@gwjh.hc.edu.tw") ,end=" ~~~ ")
+    print(b)
