3de7662c5eb3e74fb7c9f4590729881bd5771f2b
EricHuang950313 <songzhi313@gmail.com>
2021-06-25 10:30:08 +0800

    6/25

---
+++ zerojudge/a006.py
@@ -0,0 +1,9 @@
+import math
+l = input().split(" ")
+if (int(l[1])**2 - 4*int(l[0])*int(l[2])) > 0:
+    print(round((-int(l[1])+math.sqrt(int(l[1])**2 - 4*int(l[0])*int(l[2])))/(2*int(l[0]))))
+    print("Two different roots", "x1="+str(round((-int(l[1])+math.sqrt(int(l[1])**2 - 4*int(l[0])*int(l[2])))/(2*int(l[0])))), ",", "x2="+str(round((-int(l[1])-math.sqrt(int(l[1])**2 - 4*int(l[0])*int(l[2])))/(2*int(l[0])))))
+elif (int(l[1])**2 - 4*int(l[0])*int(l[2])) == 0:
+    print("Two same roots", "x="+str(round((-int(l[1])+math.sqrt(int(l[1])**2 - 4*int(l[0])*int(l[2])))/(2*int(l[0])))))
+else:
+    print("No real root")
