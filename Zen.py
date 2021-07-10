a9768953700d52d511c5c4ed25cc1dc6c7db94ef
EricHuang950313 <songzhi313@gmail.com>
2021-07-10 20:35:42 +0800

    1

--- Zen.py
+++ Zen.py
@@ -1,2 +1,5 @@
-import this
-print(this)
+with open("read.txt", mode="r", encoding="utf-8") as file:
+  for count, line in enumerate(file):
+      print(line)
+      if (count+1) % 2 == 0:
+        print()
