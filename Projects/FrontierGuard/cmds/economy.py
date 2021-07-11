fc3fb7b4be9d608d3ebdb25f6c243b78220fcbea
EricHuang950313 <songzhi313@gmail.com>
2021-07-11 12:16:21 +0800

    1

--- Projects/FrontierGuard/cmds/economy.py
+++ Projects/FrontierGuard/cmds/economy.py
@@ -7,47 +7,56 @@ class economy(Cog_Extension):
     @commands.command()
     async def register(self, ctx):
         def check_A(msgg):
-          global pasA
-          if msgg.content=="A" and msgg.channel==ctx.channel and msgg.author==ctx.author:
-            pasA = True
-            return True
-          elif msgg.content=="B" and msgg.channel==ctx.channel and msgg.author==ctx.author:
-            pasA = False
-            return True
+            global pasA
+            if msgg.content=="A" and msgg.channel==ctx.channel and msgg.author==ctx.author:
+              pasA = True
+              return True
+            elif msgg.content=="B" and msgg.channel==ctx.channel and msgg.author==ctx.author:
+              pasA = False
+              return True
         def check_B(msgg):
-          global pasB
-          pasB = True
-          with open("eco_account.txt", mode="r", encoding="utf-8") as user_file:
-            for i in user_file:
-                if msgg.content==str(i) and msgg.channel==ctx.channel and msgg.author==ctx.author:
-                    pasB = False
-                    return True
-          if pasB == True:
-            with open("eco_account.txt", mode="a", encoding="utf-8") as user_file:
-              user_file.write(msgg.content)
-            return True
-          
+            global pasB
+            pasB = True
+            with open("./economyData/eco_account.json", mode="r", encoding="utf-8") as file:
+                data = json.load(file)
+                for i in data:
+                    if msgg.content==data[i] and msgg.channel==ctx.channel and msgg.author==ctx.author or str(msgg.author.id)==str(i) and msgg.channel==ctx.channel and msgg.author==ctx.author:
+                        pasB = False
+                        return True
+            if pasB == True:
+              with open("./economyData/eco_account.json", mode="w", encoding="utf-8") as file:
+                  data[str(ctx.author.id)] = str(msgg.content)
+                  json.dump(data, file, indent=4)
+              return True
 
-        await ctx.send("Please enter: \"A\" to start the process./\"B\" to leave.")
+        await ctx.send("Please enter: \"A\" to start the process. \"B\" to leave.")
         try:
-          await self.bot.wait_for(event="message",check=check_A, timeout=15)
+            await self.bot.wait_for(event="message",check=check_A, timeout=15)
         except asyncio.TimeoutError:
-          await ctx.send("Register failed!")
+            await ctx.send("Register failed!")
         else:
-          if pasA == True:
-            await ctx.send("Please enter your user name.")
-            try:
-              await self.bot.wait_for(event="message",check=check_B, timeout=60)
-            except asyncio.TimeoutError:
-              await ctx.send("Register failed!")
-            else:
-              if pasB == False:
-                await ctx.send("Account already existed.")
-              elif pasB == True:
-                await ctx.send("Register successfily.")
-          elif pasA == False:
-            await ctx.send("Already leaved.")
-
+            if pasA == True:
+                await ctx.send("Please enter your user name(Name can not be changed!).")
+                try:
+                    await self.bot.wait_for(event="message",check=check_B, timeout=60)
+                except asyncio.TimeoutError:
+                    await ctx.send("Register failed!")
+                else:
+                    if pasB == True:
+                        await ctx.send("Register successfily.")
+                    elif pasB == False:
+                        await ctx.send("You have already registered an Account.")
+            elif pasA == False:
+                await ctx.send("Already leaved.")
+    @commands.command()
+    async def allaccountdata(self, ctx):
+        with open("./economyData/eco_account.json", mode="r", encoding="utf-8") as file:
+                data = json.load(file)
+                for i in data:
+                    if i == "Test_S":
+                        continue
+                    else:
+                        await ctx.send("==BANK Accounts==```"+data[i]+"```")
 
 def setup(bot):
     bot.add_cog(economy(bot))
