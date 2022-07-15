8cba97e17dd48cd7612755d483f8f50c49733078
EricHuang950313 <songzhi313@gmail.com>
2022-07-15 11:59:33 +0800

    Update computer.py

--- Folders/prisonersRiddle/computer.py
+++ Folders/prisonersRiddle/computer.py
@@ -8,14 +8,13 @@ def run(p:tuple, j:int, goal:int):
   loop += [j+1]
   if p[j] == goal:
     return str(j+1)  # return if it meets the target(goal)
-  return str(j+1) + "/" + run(p, p[j]-1, goal) # complete the loops and  return the whole loop
+  return str(j+1) + "/" + run(p, p[j]-1, goal) # complete the loops and return the whole loop
 
 
 if __name__ == "__main__":
   amount = int(input("Prisoners amount:"))
   prisoners = [i for i in range(1, amount+1)]
   prisoners = list(permutations(prisoners))
-  # loops = [[] for i in range(len(prisoners))]  # be observed when testing in small range
   statistics = [0, 0]  # [<=amount/2, >amount/2]
   statistics_detail = [0 for i in range(amount)]
   with open("result.txt", mode="w") as f:
@@ -25,7 +24,6 @@ if __name__ == "__main__":
       for j in range(len(prisoners[i])):
         if j+1 in loop:  # (1)
           continue
-        # loops[i] += [run(prisoners[i], j, j+1)]   # be observed when testing in small range
         loops += [run(prisoners[i], j, j+1)]  # (2)
       for k in loops:  # (2)
         if len(k.split("/")) > (amount/2):
@@ -35,8 +33,11 @@ if __name__ == "__main__":
         statistics[0] += 1
       statistics_detail[len(max(loops, key=len).split("/"))-1] += 1
       f.write(str(loops) + "\n")  # save the process into result.txt
-  print(f"{statistics} {statistics_detail}\n{[i/sum(statistics) for i in statistics]} {[i/sum(statistics_detail) for i in statistics_detail]}")
-  print(f"--> P(success)=1-{statistics[1]/sum(statistics):.2f}={1-(statistics[1]/sum(statistics)):.2f}")
+  print(f"""{statistics} {statistics_detail}\n
+            {[i/sum(statistics) for i in statistics]} 
+            {[i/sum(statistics_detail) for i in statistics_detail]}""")
+  print(f"""--> P(success)=1-{statistics[1]/sum(statistics):.2f}
+        ={1-(statistics[1]/sum(statistics)):.2f}""")
 
   plt.figure(num=f"Prisoners-{amount}")
   plt.bar([i+1 for i in range(amount)], [i/sum(statistics_detail) for i in statistics_detail], width=0.3)
